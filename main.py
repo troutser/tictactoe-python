e9338cb55de918a5425f0673810fcf6373a06814
troutser <LargeFargeMan@gmail.com>
2025-01-19 13:12:43 -0800

    Added tic tac toe functionality, removed physicsobject class, consolidated tkinter into classes

--- main.py
+++ main.py
@@ -1,49 +1,20 @@
-import tkinter as tk
-from modules.classes import PhysicsObject
+from modules.classes import RootObject
+from modules.classes import TicTacToeCell
 from modules.classes import Vector2
 
-ROOT_WIDTH, ROOT_HEIGHT = 1000, 500
-RADIUS, STARTING_POSITION = 30, Vector2(500,0)
+PADDING, GRID_SIZE, CELL_SIZE = Vector2(10,10), Vector2(3,3), Vector2(100,100)
 
-Root = tk.Tk()
-Root.geometry(f"{ROOT_WIDTH}x{ROOT_HEIGHT}")
+rootObject = RootObject()
+cells = []
+for x in range(GRID_SIZE.x):
+    for y in range(GRID_SIZE.y):
+        grid_position = Vector2(x,y)
+        screen_position = grid_position * CELL_SIZE
+        padding = PADDING * grid_position 
+        cells.append(TicTacToeCell(
+            rootObject,
+            screen_position + padding,
+            CELL_SIZE,
+        ))
 
-Canvas = tk.Canvas(
-    Root,
-    width=ROOT_WIDTH, 
-    height=ROOT_HEIGHT,
-    bg="white"
- )
-Canvas.pack()
-
-Circle = Canvas.create_oval(
-    STARTING_POSITION.x, 
-    STARTING_POSITION.y,
-    RADIUS+STARTING_POSITION.x,
-    RADIUS+STARTING_POSITION.y, 
-    fill="blue"
-)
-Physics = PhysicsObject(
-    position = STARTING_POSITION,
-    velocity = Vector2(0,0),
-    acceleration = Vector2(0,1)
-)
-def Step():
-    Physics.step(0.01)
-    if Physics.position.y > ROOT_HEIGHT - RADIUS:
-        Physics.position.y = ROOT_HEIGHT * 2 - RADIUS * 2 - Physics.position.y
-        Physics.velocity *= -0.1
-
-    Canvas.coords(
-        Circle, 
-        Physics.position.x, 
-        Physics.position.y,
-        RADIUS+Physics.position.x, 
-        RADIUS+Physics.position.y
-    )
-    print(Canvas.coords(Circle)[1])
-    Root.after(30, Step)
-
-
-Step()
-Root.mainloop()
+rootObject.start()
--- modules/classes.py
+++ modules/classes.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
-import math as math
+import tkinter as tk
+from tkinter import PhotoImage
 
 class Vector2:
     def __init__(self, x: float, y: float):
@@ -8,33 +9,70 @@ class Vector2:
     def __str__(self):
         return f"({self.x}, {self.y})"
     def __add__(self, vec2: Vector2) -> Vector2:
-        self.x = self.x + vec2.x
-        self.y = self.y + vec2.y
-
-        return self
-    def __mul__(self, scalar: float) -> Vector2:
-        self.x = self.x * scalar
-        self.y = self.y * scalar
-
-        return self
+        return Vector2(self.x + vec2.x, self.y + vec2.y)
+    def __mul__(self, multiplier: float | Vector2) -> Vector2:
+        if isinstance(multiplier, float):
+            return Vector2(self.x * multiplier, self.y * multiplier)
+        elif isinstance(multiplier, Vector2):
+            return Vector2(self.x * multiplier.x, self.y * multiplier.y)
     def __truediv__(self, divisor: float) -> Vector2:
-        self = self*(1/divisor)
-
-        return self
+        return self*(1/divisor)
     def __sub__(self, vec2):
-        self = self + -1 * vec2
-
-        return self
+        return self + -1 * vec2
     def dot(self, vec2):
         return self.x * vec2.x + self.y * vec2.y
-    
-class PhysicsObject:
-    def __init__(self, position: Vector2, velocity: Vector2, acceleration: Vector2):
-        self.position = position
-        self.velocity = velocity
-        self.acceleration = acceleration
-    def step(self, dt: float) -> PhysicsObject:
-        self.position += self.velocity
-        self.velocity += self.acceleration
+
+class RootObject:
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.state("zoomed")
+
+        self.canvas = tk.Canvas(
+            self.root,
+            width=self.root.winfo_screenwidth(),
+            height=self.root.winfo_screenheight(),
+            bg="black"
+        )
+        self.canvas.pack()
+    def start(self):
+        self.root.mainloop()
+
+class TicTacToeCell:
+    def __init__(self, rootObject: RootObject, position: Vector2, size: Vector2):
+        self.tag = f"cell_{position.x}_{position.y}"
+        self.rootObject = rootObject
+
+        self.images = {
+            'empty': PhotoImage(file="images/empty_cell.png"),
+            'o': PhotoImage(file="images/o_cell.png"),
+            'x': PhotoImage(file="images/x_cell.png")
+        }
+        self._state = "empty"
+
+        self.UI_element = rootObject.canvas.create_image(
+            position.x + size.x / 2, position.y + size.y / 2, image=self.images['empty'], tags=self.tag
+        )
+
+        rootObject.canvas.tag_bind(self.tag, "<Button-1>", self.on_left_click)
+        rootObject.canvas.tag_bind(self.tag, "<Button-2>", self.on_middle_click)
+        rootObject.canvas.tag_bind(self.tag, "<Button-3>", self.on_right_click)
+
+    @property
+    def state(self):
+        return self._state
+    @state.setter
+    def state(self, value):
+        if self._state == value:
+            return 
         
-        return self
+        self._state = value
+        self.update_image()
+
+    def update_image(self):
+        self.rootObject.canvas.itemconfig(self.UI_element, image=self.images[self.state])
+    def on_left_click(self, event):
+        self.state = 'x'
+    def on_right_click(self, event):
+        self.state = 'o'
+    def on_middle_click(self, event):
+        self.state = 'empty'
