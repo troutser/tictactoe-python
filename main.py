b12fc410107d34ca174ccb4979ae529ea4c44729
troutser <LargeFargeMan@gmail.com>
2025-01-18 18:45:48 -0800

    Made modules folder, moved classes to modules

--- main.py
+++ main.py
@@ -1,21 +1,3 @@
-import tkinter as tk
+from modules.classes import Vector2
 
-class Vector2:
-    def __init__(self, x,y):
-        self.x = x
-        self.y = y
-    def __str__(self):
-        return f"({self.x}, {self.y})"
-    def add(self, vec2):
-        x = self.x + vec2.x
-        y = self.y + vec2.y
-    def mult(self, scalar):
-        x = self.x * scalar
-        y = self.y * scalar
-    def div(self, divisor):
-        self = self.mult(1/divisor)
-    def sub(self, vec2):
-        self = self.add(-vec2)
-
-testVector = Vector2(3,1)
-print(testVector.add(Vector2(1,2)))
+print("here")
---
+++ modules/classes.py
@@ -0,0 +1,29 @@
+from __future__ import annotations
+import math as math
+
+class Vector2:
+    def __init__(self, x: float, y: float):
+        self.x = x
+        self.y = y
+    def __str__(self):
+        return f"({self.x}, {self.y})"
+    def __add__(self, vec2: Vector2) -> Vector2:
+        self.x = self.x + vec2.x
+        self.y = self.y + vec2.y
+
+        return self
+    def __mul__(self, scalar: float) -> Vector2:
+        self.x = self.x * scalar
+        self.y = self.y * scalar
+
+        return self
+    def __truediv__(self, divisor: float) -> Vector2:
+        self = self*(1/divisor)
+
+        return self
+    def __sub__(self, vec2):
+        self = self + -1 * vec2
+
+        return self
+    def dot(self, vec2):
+        return self.x * vec2.x + self.y * vec2.y
